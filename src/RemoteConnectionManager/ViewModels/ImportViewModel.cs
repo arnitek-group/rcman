6697e18280cb150d6c7f17ea2365132cc769d9f2
Nikolay Arhangelov <nikolay.arhangelov@hotmail.com>
2017-11-13 20:37:20 +0200

    Added tracking for import.

--- src/RemoteConnectionManager/ViewModels/ImportViewModel.cs
+++ src/RemoteConnectionManager/ViewModels/ImportViewModel.cs
@@ -1,6 +1,8 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using RemoteConnectionManager.Core.Connections;
+using RemoteConnectionManager.Core.Services;
 using RemoteConnectionManager.Extensions;
 using RemoteConnectionManager.ExternalProcess;
 using RemoteConnectionManager.Properties;
@@ -9,12 +11,15 @@ using RemoteConnectionManager.Services;
 
 namespace RemoteConnectionManager.ViewModels
 {
-    public class ImportViewModel: ViewModelBase
+    public class ImportViewModel : ViewModelBase
     {
         private readonly IDialogService _dialogService;
-        public ImportViewModel(IDialogService dialogService)
+        private readonly ITelemetryService _telemetryService;
+
+        public ImportViewModel(IDialogService dialogService, ITelemetryService telemetryService)
         {
             _dialogService = dialogService;
+            _telemetryService = telemetryService;
 
             ImportRdpFileCommand = new RelayCommand(ExecuteImportRdpFileCommand);
             ImportPuttySessionsCommand = new RelayCommand(ExecuteImportPuttySessionsCommand);
@@ -45,6 +50,12 @@ namespace RemoteConnectionManager.ViewModels
                 items.ForEach(x => ViewModelLocator.Locator.Main.Items.Add(new CategoryItemViewModel(x, null)));
                 ViewModelLocator.Locator.Main.SuspendSave = false;
                 ViewModelLocator.Locator.Main.SaveConnections();
+
+                _telemetryService.TrackEvent("Import", new Dictionary<string, string>
+                {
+                    {"Protocol", items[0].ConnectionSettings.Protocol.ToString().ToUpper()},
+                    {"Count", items.Length.ToString()}
+                });
             }
 
             _dialogService.ShowInfoDialog(string.Format(Resources.InfoImport, items.Length));
