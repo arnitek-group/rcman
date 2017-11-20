6dc606f97d0f1fbd3113cbfda631c53962569055
Nikolay Arhangelov <nikolay.arhangelov@hotmail.com>
2017-11-20 18:21:50 +0200

    Implemented connections file settings.

---
+++ src/RemoteConnectionManager/UserControls/PropertyFileSave.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace RemoteConnectionManager.UserControls
+{
+    public partial class PropertyFileSave : UserControl
+    {
+        public PropertyFileSave()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.FileName = Path.GetFileName(TextBox_File.Text);
+            dialog.Filter = "*.json|*.json";
+
+            if (dialog.ShowDialog() == true)
+            {
+                TextBox_File.Clear();
+                TextBox_File.AppendText(dialog.FileName);
+                TextBox_File.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
+        }
+    }
+}
