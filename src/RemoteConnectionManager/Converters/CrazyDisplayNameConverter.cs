459c08038cb784304d9f54c1b29b147b6e4ecb55
Nikolay Arhangelov <nikolay.arhangleov@mentormate.com>
2018-05-11 14:44:57 +0300

    Added a try catch block to avoid an error that I can't reproduce, but has been popping up sporadically.

--- src/RemoteConnectionManager/Converters/CrazyDisplayNameConverter.cs
+++ src/RemoteConnectionManager/Converters/CrazyDisplayNameConverter.cs
@@ -10,21 +10,25 @@ using Xceed.Wpf.AvalonDock.Layout;
 
 namespace RemoteConnectionManager.Converters
 {
-    public class CrazyDisplayNameConverter:IValueConverter
+    public class CrazyDisplayNameConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var textBlock = (TextBlock) value;
-            var connectionSettings = ((IConnection)((LayoutDocument)textBlock.DataContext).Content).ConnectionSettings;
-            var civm = ViewModelLocator.Locator
-                .Main.Items
-                .GetFlatList(x => x.Items, x => x.ConnectionSettings != null)
-                .Single(x => x.CategoryItem.ConnectionSettings == connectionSettings);
+            try
+            {
+                var textBlock = (TextBlock)value;
+                var connectionSettings = ((IConnection)((LayoutDocument)textBlock.DataContext).Content).ConnectionSettings;
+                var civm = ViewModelLocator.Locator
+                    .Main.Items
+                    .GetFlatList(x => x.Items, x => x.ConnectionSettings != null)
+                    .Single(x => x.CategoryItem.ConnectionSettings == connectionSettings);
 
-            var binding = new Binding("DisplayName");
-            binding.Source = civm;
+                var binding = new Binding("DisplayName");
+                binding.Source = civm;
 
-            textBlock.SetBinding(TextBlock.TextProperty, binding);
+                textBlock.SetBinding(TextBlock.TextProperty, binding);
+            }
+            catch { }
 
             return null;
         }
