f0708f813d9bf9a1642d6c2659f96ee5a6f923c7
Nikolay Arhangelov <nikolay.arhangelov@arnitek.com>
2019-09-17 10:57:06 +0300

    Updated portfolio links.

--- src/RemoteConnectionManager/Properties/Resources.Designer.cs
+++ src/RemoteConnectionManager/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RemoteConnectionManager.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -250,7 +250,7 @@ namespace RemoteConnectionManager.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://www.arnitek.com/projects/rcman/#feedback.
+        ///   Looks up a localized string similar to https://www.arnitek.com/portfolio/rcman/#feedback.
         /// </summary>
         public static string FeedbackLink {
             get {
@@ -421,7 +421,7 @@ namespace RemoteConnectionManager.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://www.arnitek.com/projects/rcman/releases/.
+        ///   Looks up a localized string similar to https://www.arnitek.com/portfolio/rcman/#releases.
         /// </summary>
         public static string Releases {
             get {
